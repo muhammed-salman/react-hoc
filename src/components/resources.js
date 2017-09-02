79fa0584ecd8be0cce9ecf3c8c65d8e93ce1a140
Muhammed Salman <muhammed-salman@users.noreply.github.com>
2017-09-02 20:21:46 +0530

    React Higher Order Component

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default ()=>{
+	return(
+		<div>
+			<h3>Secret Sauce Recipe</h3>
+			<ul>
+				<li>1 Cup Egg</li>
+				<li>1 Cup Oil</li>
+				<li>1 cup Sugar</li>
+			</ul>
+		</div>	
+	);
+}
